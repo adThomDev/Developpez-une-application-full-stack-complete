3affe06e2066e60d929e20bb7d0e054eec0ab22c
adThomDev <gretatpcda@gmail.com>
2025-03-21 16:29:44 +0100

    WiP on sidebar

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule, MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule]
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatDrawer,
+  ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isSmallScreen: boolean = false;
+  @ViewChild('drawer') drawer!: MatDrawer;
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize();
+  }
 
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isSmallScreen = window.innerWidth <= 500;
+  }
+
+  toggleSidePanel() {
+    if (this.drawer) this.drawer.toggle();
+  }
+}
