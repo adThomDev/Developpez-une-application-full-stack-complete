9828c86784e211f17a690ff95480f162981bf9fe
adThomDev <gretatpcda@gmail.com>
2025-03-22 17:54:28 +0100

    User profile component

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ThemesComponent } from './pages/themes/themes.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'themes', component: ThemesComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: UserProfileComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- front/src/app/pages/user-profile/user-profile.component.ts
+++ front/src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserService } from 'src/services/userService';
+import { ThemeService } from 'src/services/themeService';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: any = {
+    username: '',
+    email: '',
+    subscribedThemes: []
+  };
+  themes: any[] = [];
 
+  constructor(private userService: UserService, private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe((data) => {
+      this.user = data;
+    });
+
+    this.themeService.getThemes().subscribe((data) => {
+      this.themes = data;
+    });
+  }
+
+  isSubscribed(themeId: number): boolean {
+    return this.user?.subscribedThemes.includes(themeId);
+  }
+
+  unsubscribe(themeId: number): void {
+    this.user.subscribedThemes = this.user.subscribedThemes.filter((id: number) => id !== themeId);
+  }
+
+  saveProfile(): void {
+    console.log('Profile saved:', this.user);
+    // TODO
+  }
 }
