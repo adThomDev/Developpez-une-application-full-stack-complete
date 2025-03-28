00171eea0e2439fd3698ada7e8b02f06f9597f3f
adThomDev <gretatpcda@gmail.com>
2025-03-28 15:50:07 +0100

    Added CORS settings for controllers

--- back/src/main/java/com/openclassrooms/mddapi/controllers/ArticleController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/ArticleController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/article")
 public class ArticleController {
