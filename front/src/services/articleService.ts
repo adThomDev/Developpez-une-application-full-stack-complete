6aed46e2d05374fd6628ceccf8da34184dc12bcf
adThomDev <gretatpcda@gmail.com>
2025-03-28 09:32:00 +0100

    WiP on API URL

--- front/src/services/articleService.ts
+++ front/src/services/articleService.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { BASE_URL } from 'src/app/constants/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
-  private articlesUrl = 'assets/mocks/articles.json';
+  // private articlesUrl = 'assets/mocks/articles.json';
+  private articlesUrl = `${BASE_URL}/article`;
 
   constructor(private http: HttpClient) {}
 
