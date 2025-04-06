c816c0c3a93cf56fafb3ae1d0b1ed470ab4145a0
adThomDev <gretatpcda@gmail.com>
2025-04-06 11:04:30 +0200

    Updated postman collection

--- back/src/main/java/com/openclassrooms/mddapi/controllers/ArticleController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/ArticleController.java
@@ -3,16 +3,20 @@ package com.openclassrooms.mddapi.controllers;
 import com.openclassrooms.mddapi.models.DTOs.ArticleDTO;
 import com.openclassrooms.mddapi.models.DTOs.CommentaryDTO;
 import com.openclassrooms.mddapi.models.DTOs.CommentaryRequestDTO;
+import com.openclassrooms.mddapi.models.DTOs.CreateArticleRequestDTO;
 import com.openclassrooms.mddapi.models.entities.Article;
 import com.openclassrooms.mddapi.models.entities.Commentary;
+import com.openclassrooms.mddapi.models.entities.Theme;
 import com.openclassrooms.mddapi.models.entities.UserEntity;
 import com.openclassrooms.mddapi.repositories.ArticleRepository;
 import com.openclassrooms.mddapi.repositories.CommentaryRepository;
+import com.openclassrooms.mddapi.repositories.ThemeRepository;
 import com.openclassrooms.mddapi.repositories.UserEntityRepository;
 import com.openclassrooms.mddapi.security.jwt.JwtUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,15 +29,18 @@ public class ArticleController {
   private final ArticleRepository articleRepository;
   private final CommentaryRepository commentaryRepository;
   private final UserEntityRepository userRepository;
+  private final ThemeRepository themeRepository;
   private final JwtUtils jwtUtils;
 
   public ArticleController(ArticleRepository articleRepository,
                            CommentaryRepository commentaryRepository,
                            UserEntityRepository userRepository,
+                           ThemeRepository themeRepository,
                            JwtUtils jwtUtils) {
     this.articleRepository = articleRepository;
     this.commentaryRepository = commentaryRepository;
     this.userRepository = userRepository;
+    this.themeRepository = themeRepository;
     this.jwtUtils = jwtUtils;
   }
 
@@ -59,7 +66,6 @@ public class ArticleController {
       @RequestBody CommentaryRequestDTO request,
       @RequestHeader("Authorization") String authHeader
   ) {
-    // Get JWT from "Bearer <token>"
     String token = authHeader.replace("Bearer ", "");
 
     if (!jwtUtils.validateJwtToken(token)) {
@@ -87,6 +93,40 @@ public class ArticleController {
     return ResponseEntity.ok().build();
   }
 
+  @PostMapping
+  public ResponseEntity<?> createArticle(
+      @RequestBody CreateArticleRequestDTO request,
+      @RequestHeader("Authorization") String authHeader
+  ) {
+    String token = authHeader.replace("Bearer ", "");
+    if (!jwtUtils.validateJwtToken(token)) {
+      return ResponseEntity.status(401).body("Invalid or expired token");
+    }
+
+    String userEmail = jwtUtils.getUserNameFromJwtToken(token);
+    Optional<UserEntity> userOpt = userRepository.findByEmail(userEmail);
+    if (userOpt.isEmpty()) {
+      return ResponseEntity.status(401).body("User not found");
+    }
+
+    Optional<Theme> themeOpt = themeRepository.findById(request.getThemeId());
+    if (themeOpt.isEmpty()) {
+      return ResponseEntity.badRequest().body("Invalid theme ID");
+    }
+
+    Article article = new Article();
+    article.setTitle(request.getTitle());
+    article.setContent(request.getContent());
+    article.setCreatedAt(new Date());
+    article.setUserEntity(userOpt.get());
+    article.setTheme(themeOpt.get());
+
+    articleRepository.save(article);
+
+    return ResponseEntity.ok().build();
+  }
+
+  //TODO à mettre dans un mapper
   private ArticleDTO convertToDTO(Article article, boolean includeCommentaries) {
     ArticleDTO articleDTO = new ArticleDTO();
     articleDTO.setId(article.getId());
